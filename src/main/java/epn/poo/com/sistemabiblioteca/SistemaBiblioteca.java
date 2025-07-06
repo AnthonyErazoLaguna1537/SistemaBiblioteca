845152d12d9f6f864cec34edb3f3b82a59cd6b13
AnthonyErazoLaguna1537 <anthonylaguna188@gmail.com>
2025-07-05 23:28:11 -0500

    InicializarRepositorio

---
+++ src/main/java/epn/poo/com/sistemabiblioteca/SistemaBiblioteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package epn.poo.com.sistemabiblioteca;
+
+/**
+ *
+ * @author Thony
+ */
+public class SistemaBiblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
